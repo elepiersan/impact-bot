0891988fbdc31a8fc00a0e97c67a5cf136090663
Marie E. Rognes <meg@simula.no>
2018-01-18 22:18:08 +0100

    Add h-index group computation.

--- impact_bot.py
+++ impact_bot.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import time
+import pickle
 
 # Import rpy2 for working with r package scholar
 import rpy2
@@ -7,20 +8,64 @@ import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 
 # Import pandas for easier working with r data tables in Python
-#from rpy2.robjects import r, pandas2ri
+import pandas
+from rpy2.robjects import r, pandas2ri
 
-def compute_impact(persons):
-    
+# Note the following output format from get_publications:
+# info[0]: Publication titles
+# info[1]: Publication authors
+# info[2]: Journal titles
+# info[3]: Page numbers
+# info[4]: Citations
+# info[5]: Year
+# info[6]: ? 
+# info[7]: ?
+
+def extract_scholar_publications(persons):
+
     # Import the scholar package
     scholar = importr("scholar")
 
-    impact = 0  
-    for (name, google_id) in persons.items():
+    # Extract scholar publication information for each person, store
+    # as standard Python dictionary
+    publications = {}
+    for (name, id) in persons.items():
 
-        # Get basic profile info (and convert to Python data
-        # structure)
-        info = scholar.get_profile(google_id)
+        # Get basic profile info
+        pubs = scholar.get_publications(id)
+
+        # Convert to pandas dataframe
+        try:
+            df = pandas2ri.ri2py_dataframe(pubs)
+            publications[id] = df
+        except:
+            pass
+            
+    return publications
+
+def extract_scholar_profile_info(persons):
+
+    # Import the scholar package
+    scholar = importr("scholar")
+
+    # Extract scholar information for each person
+    infos = {}
+    for (name, id) in persons.items():
+
+        # Get basic profile info
+        info = scholar.get_profile(id)
+
+        # Convert info to standard Python format
         info = dict(zip(info.names, list(info)))
+        infos[id] = info
+
+    return infos
+    
+def compute_impact(infos):
+
+    # Compute total number of citatons (ignoring duplicates)
+    impact = 0
+    for (id, info) in infos.items():
 
         # Extract number of total citations for given person
         print("Measuring impact of %s" % info["name"][0])
@@ -33,17 +78,52 @@ def compute_impact(persons):
         impact += num_cites
         
     return impact
-        
+
+def h_index(persons):
+
+    # Extract information from scholar
+    publications = extract_scholar_publications(persons)
+    #pickle.dump(publications, open("publications.pickle", "wb"))
+    #exit()
+    #publications = pickle.load(open("publications.pickle", "rb"))
+
+    titles = {}
+
+    # Extract unique titles and corresponding number of citations
+    for (id, pubs) in publications.items():
+        for index, title in pubs["title"].iteritems(): 
+            titles[title] = pubs["cites"][index]
+
+    # Sort list of num of citations in descending order
+    cites = titles.values()
+    cites.sort(reverse=True)
+
+    # Compute the h-index
+    for (i, num_cites) in enumerate(cites):
+        if (i+1) > num_cites:
+            break
+    h_index = i
+
+    return h_index
+    
+def impact(persons):
+
+    # Extract basic profile from scholar
+    infos = extract_scholar_profile_info(persons)
+    
+    # Compute impact
+    total_impact = compute_impact(infos)
+    return total_impact
+    
 if __name__ == "__main__":
 
     # Import input data
     from people import persons
 
-    # Compute impact
-    total_impact = compute_impact(persons)
+    pandas2ri.activate()
     
-    print("Your impact today is %d. Well done!" % total_impact)
+    print("Your impact today is %d. Well done!" % impact(persons))
+    print("Your h-index today is %d. Well done!" % h_index(persons))
 
-    
 
     
